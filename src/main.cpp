d3abc113ee27d277cdb4d21c05faa1155e1aa3ab
Dan Jose <16danjj@gmail.com>
2026-06-22 18:25:04 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@ int main() {
 
   if (command == "exit"){
     break;
+  } else if (command.substr(0,5) == "echo ") {
+    std::cout << command.substr(6) << std::endl;
   }
 
   std::cout << command << ": command not found" << std::endl; }
