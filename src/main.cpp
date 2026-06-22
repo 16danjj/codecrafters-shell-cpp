7c7c8c80ed09a68b85521d273c03a0d762ccb72a
Dan Jose <16danjj@gmail.com>
2026-06-22 18:30:29 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,10 @@ int main() {
     break;
   } else if (command.substr(0,5) == "echo ") {
     std::cout << command.substr(5) << std::endl;
-    continue;
+  } else {
+    std::cout << command << ": command not found" << std::endl;
   }
 
-  std::cout << command << ": command not found" << std::endl; }
+  }
 
 }
