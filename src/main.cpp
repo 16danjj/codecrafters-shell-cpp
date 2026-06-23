af82a026f01b0a765e1b8237006076584665c77d
Dan Jose <16danjj@gmail.com>
2026-06-23 10:35:10 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,40 +13,47 @@ int main() {
   string command;
   
   while (1) {
-  cout << "$ ";
+    cout << "$ ";
 
-  getline(cin, command);
+    getline(cin, command);
+    bool file_found = false;
+    
+    if (command == "exit"){
+      break;
+    } else if (command.substr(0,5) == "type ") {
+      if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type") {
+        cout << command.substr(5) << " is a shell builtin" << endl;
+      } else {
 
-  
-  if (command == "exit"){
-    break;
-  } else if (command.substr(0,5) == "type ") {
-    if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type") {
-      cout << command.substr(5) << " is a shell builtin" << endl;
-    } else {
+        string path = getenv("PATH");
+        stringstream ss(path);
+        string unique_path;
 
-      string path = getenv("PATH");
-      stringstream ss(path);
-      string unique_path;
 
-      while (getline(ss,unique_path, ':')) {
-        string full_path = unique_path + "/" + command.substr(5);
+        while (getline(ss,unique_path, ':')) {
 
-        if (access(full_path.c_str(), X_OK) == 0) {
-          cout << command.substr(5) << " is " << full_path << endl;
-          return 0;
+          string full_path = unique_path + "/" + command.substr(5);
+
+          if (access(full_path.c_str(), X_OK) == 0) {
+            cout << command.substr(5) << " is " << full_path << endl;
+            file_found = true;
+            break;
+          }
+
         }
 
+        if (!file_found) {
+          cout << command.substr(5) << ": not found" << endl;
+        }
+        
       }
-      cout << command.substr(5) << ": not found" << endl;
     }
-  }
-  
-  else if (command.substr(0,5) == "echo ") {
-    cout << command.substr(5) << endl;
-  } else {
-    cout << command << ": command not found" << endl;
-  }
+    
+    else if (command.substr(0,5) == "echo ") {
+      cout << command.substr(5) << endl;
+    } else {
+      cout << command << ": command not found" << endl;
+    }
 
   }
 
