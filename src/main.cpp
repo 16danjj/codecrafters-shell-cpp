2b464645e77fe8ad3cac68ee5ca683ac0f8bb64c
Dan Jose <16danjj@gmail.com>
2026-06-23 10:20:50 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
-
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -15,16 +15,24 @@ int main() {
 
   std::getline(std::cin, command);
 
-  std::string path = getenv("PATH");
-  std::cout << path << std::endl;
-  break;
-
+  
   if (command == "exit"){
     break;
   } else if (command.substr(0,5) == "type ") {
     if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type") {
       std::cout << command.substr(5) << " is a shell builtin" << std::endl;
     } else {
+
+      std::string path = getenv("PATH");
+      std::stringstream ss(path);
+
+      std::string unique_path;
+
+      while (getline(ss,unique_path, ':')) {
+        std::cout << unique_path << std::endl;
+      }
+
+
       std::cout << command.substr(5) << ": not found" << std::endl;
     }
   }
