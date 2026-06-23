0a5073e4b6269c13cd7a322c79b15bd3c5abb409
Dan Jose <16danjj@gmail.com>
2026-06-23 10:23:20 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,48 @@
 #include <string>
 #include <filesystem>
 #include <sstream>
+using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  // Flush after every cout / std:cerr
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  std::string command;
+  string command;
   
   while (1) {
-  std::cout << "$ ";
+  cout << "$ ";
 
-  std::getline(std::cin, command);
+  getline(cin, command);
 
   
   if (command == "exit"){
     break;
   } else if (command.substr(0,5) == "type ") {
     if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type") {
-      std::cout << command.substr(5) << " is a shell builtin" << std::endl;
+      cout << command.substr(5) << " is a shell builtin" << endl;
     } else {
 
-      std::string path = getenv("PATH");
-      std::stringstream ss(path);
+      string path = getenv("PATH");
+      stringstream ss(path);
 
-      std::string unique_path;
+      string unique_path;
 
       while (getline(ss,unique_path, ':')) {
-        std::cout << unique_path << std::endl;
+        string full_path = unique_path + command.substr(5);
+        cout << full_path << endl;
+
       }
 
 
-      std::cout << command.substr(5) << ": not found" << std::endl;
+      cout << command.substr(5) << ": not found" << endl;
     }
   }
   
   else if (command.substr(0,5) == "echo ") {
-    std::cout << command.substr(5) << std::endl;
+    cout << command.substr(5) << endl;
   } else {
-    std::cout << command << ": command not found" << std::endl;
+    cout << command << ": command not found" << endl;
   }
 
   }
