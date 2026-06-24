61f5432bc44867a7f07f319e709f8035d7ca0901
Dan Jose <16danjj@gmail.com>
2026-06-24 19:26:49 +0200

    codecrafters submit [skip ci]

--- src/cd.hpp
+++ src/cd.hpp
@@ -14,14 +14,14 @@ namespace shell::cd {
 
         int result;
 
-        if (input.rfind("/", 0) == 0)  { // absolute path
 
-            result = chdir(input.c_str());
 
-            if (result != 0) {
-                cout << "cd: " << input << ": No such file or directory" << endl;
-            }
+        result = chdir(input.c_str());
+
+        if (result != 0) {
+            cout << "cd: " << input << ": No such file or directory" << endl;
         }
+
         
         return 0;
     }
