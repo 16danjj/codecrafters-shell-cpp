7a1aa438e457bfd3ab4b573f158468e657dba55f
Dan Jose <16danjj@gmail.com>
2026-06-25 21:22:45 +0200

    codecrafters submit [skip ci]

--- src/command_handler.hpp
+++ src/command_handler.hpp
@@ -97,12 +97,15 @@ namespace shell {
             quote_sanitised = pattern_match_handling(input, pattern_double_quote, "double");
         }
 
+        quote_sanitised = quote_sanitised.empty()? input : quote_sanitised;
+
         if (quote_sanitised.find('\'') != string::npos) {
             quote_sanitised = pattern_match_handling(quote_sanitised, pattern_single_quote, "single");
         } 
 
-        if (quote_sanitised.empty()) {
-            quote_sanitised += remove_whitespace(input);
+        if (quote_sanitised == input) {
+            input = remove_whitespace(input);
+            return;
         }
 
         input = quote_sanitised;
